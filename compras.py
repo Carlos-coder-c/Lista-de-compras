eab01e3063463b258f81b93f9a63429e78edffe8
Carlos Eduardo <menino.podsonhar@gmail.com>
2026-02-27 09:07:28 -0300

    Alterações de Erros: Indentation e Tabs errados

--- compras.py
+++ compras.py
@@ -1,36 +1,33 @@
-escolha = input('Selecione: \
-[i]nserir, [a]pagar, [l]ista, [s]air').lower()
-
-
 lista_compras = []
 
 
 
 while True:
+    escolha = input('Selecione: \
+    [i]nserir, [a]pagar, [l]ista, [s]air: ').lower()
  
     if escolha == 'i':
         item = input('Digite o item: ')
         lista_compras.append(item)
-    try:
-      if escolha == 'a':
-         apagar_item = int(input('Digite o indice: '))
-         del lista_compras[apagar_item]
+ try:
+    if escolha == 'a':
+      apagar_item = int(input('Digite o indice: '))
+      del lista_compras[apagar_item]
 
-    except ValueError:
+ except ValueError:
         print('Digite apenas inteiros!')
-    except Exception:
+ except Exception:
         print('Erro desconhecido')
 
-    if escolha == 'l':
-      for indice, itens in lista_compras:
+    elif escolha == 'l':
+      for indice, itens in enumerate(lista_compras):
         print(indice, itens)
         break
 
     elif escolha == 's':
         break
 
-    else:
-        print('Digite apenas s l, a ou i')
+
 
 if len(escolha) < 1:
     print('Digite apenas 1 caracter')
